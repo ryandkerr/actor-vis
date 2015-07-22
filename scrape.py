1f8528e149d683e90981bf86d3aa2394f747a79b
ryandkerr <ryankerr@college.harvard.edu>
2015-07-22 00:04:50 -0400

    filters credits to only count movies

--- scrape.py
+++ scrape.py
@@ -8,6 +8,7 @@ import re
 
 
 tom_hanks = "http://www.imdb.com/name/nm0000158/?ref_=nv_sr_1"
+not_movies = ["filming", "pre-production", "post-production", "TV", "Short", "Game"]
 
 def make_soup(url):
   html = urlopen(url).read()
@@ -16,7 +17,19 @@ def make_soup(url):
 
 soup = make_soup(tom_hanks)
 
-movies = soup.find_all("div", id=re.compile("^actor-"))
+credits = soup.find_all("div", id=re.compile("^actor-"))
 # .find("div", "filmo-category-section")
 
+movies = []
+for credit in credits:
+    movie = True
+    for n in not_movies:
+        if (movie == False) or (re.search(n, credit.get_text()) != None):
+            movie = False
+    if movie:
+        movies.append(credit)
+
+
+
+# short = re.search("(TV Short)", movies[5].get_text()) != None
 
