a088526e1ff245a5a6a30f85e106366d0b684343
ryandkerr <ryankerr@college.harvard.edu>
2015-08-27 02:06:33 -0400

    Add actor-select minimal working functionality

--- scrape.py
+++ scrape.py
@@ -7,13 +7,17 @@ import re
 import os
 
 # classifiers for non-movies, not included in scraping
-not_movies = ["filming", "pre-production", "post-production", "Short", "Game\)",
-              "Mini", "TV", "completed", "announced"]
+not_movies = ["filming", "pre-production", "post-production", "Short",
+              "Mini", "TV", "completed", "announced", "Video"]
 
 # define actors and IMDB urls
-actors = {"Tom Hanks": "http://www.imdb.com/name/nm0000158/?ref_=nv_sr_1",
-          "Jennifer Lawrence": "http://www.imdb.com/name/nm2225369/"}
-
+actors = {"Harrison Ford": "http://www.imdb.com/name/nm0000148/"}
+# "Michael Caine": "http://www.imdb.com/name/nm0000323/?ref_=tt_cl_t4"
+# "Chris Pratt": "http://www.imdb.com/name/nm0695435/?ref_=nv_sr_1"
+# "Anne Hathaway": "http://www.imdb.com/name/nm0004266/?ref_=nv_sr_1"
+# "Morgan Freeman": "http://www.imdb.com/name/nm0000151/?ref_=nv_sr_2"
+# "Tom Hanks": "http://www.imdb.com/name/nm0000158/?ref_=nv_sr_1",
+# "Jennifer Lawrence": "http://www.imdb.com/name/nm2225369/",
 
 def make_soup(url):
   html = urlopen(url).read()
